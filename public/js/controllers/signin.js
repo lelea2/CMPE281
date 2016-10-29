f757d7d8e64121fe3f323b024b48f30a4c1c0b15
lelea2 <daomaikhanh1988@yahoo.com>
2016-10-29 13:02:23 -0700

    initial check i

--- public/js/controllers/signin.js
+++ public/js/controllers/signin.js
@@ -6,11 +6,27 @@ App.controller('signinController', ['$scope', '$http', function ($scope, $http) 
   $scope.signinState = true;
 
   $scope.processSignin = function() {
-
+    $http({
+      method  : 'POST',
+      url     : '/api/login',
+      data    : $.param($scope.formSigninData)
+    }).then(function(data) {
+      window.location = '/dashboard'; //redirect to dashboard
+    }, function(err) {
+      alert('Fail to login. Please try again');
+    });
   };
 
   $scope.processSignup = function() {
-
+    $http({
+      method  : 'POST',
+      url     : '/api/accounts',
+      data    : $.param($scope.formSignupData)
+    }).then(function(data) {
+      window.location = '/dashboard'; //redirect to dashboard
+    }, function(err) {
+      alert('Fail to login. Please try again');
+    });
   };
 
   $scope.showSignin = function() {
