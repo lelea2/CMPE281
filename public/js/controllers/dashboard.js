2424af3eca367439c182ef0b372b7026cb66ab43
lelea2 <daomaikhanh1988@yahoo.com>
2016-11-30 17:25:37 -0800

    Initial check in

--- public/js/controllers/dashboard.js
+++ public/js/controllers/dashboard.js
@@ -1,4 +1,33 @@
 App.controller('dashboardController', ['$scope', '$http', function ($scope, $http) {
 
+  $scope.sensor_type = {};
+
+  $scope.init = function() {
+    //Draw sensor type
+    $http({
+      method: 'GET',
+      headers: APP_CLOUD.getHeaders(true),
+      url: '/api/sensors'
+    }).then(function(resp) {
+      $scope.sensor_type = resp.data.metadata;
+      Morris.Donut({
+        element: 'morris-donut-chart1',
+        data: [{
+            label: "Temperature Sensor",
+            value: data.temperature
+        }, {
+            label: "Location Sensor",
+            value: data.location
+        }, {
+            label: "Speed Sensor",
+            value: data.speed
+        },{
+            label: "Clipper Sensor",
+            value: data.clipper
+        }],
+        resize: true
+      });
+    });
+  }
 
 }]);
