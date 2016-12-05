e606131b59125825ebe6ccb28650cb27b9818751
lelea2 <daomaikhanh1988@yahoo.com>
2016-12-04 17:11:51 -0800

    Initial check in

--- public/js/controllers/billings.js
+++ public/js/controllers/billings.js
@@ -1,5 +1,16 @@
 App.controller('billingsController', ['$scope', '$http', function ($scope, $http) {
 
+  $scope.billings = [];
 
+  $scope.init = function() {
+    $http({
+      method: 'GET',
+      headers: APP_CLOUD.getHeaders(true),
+      url: '/api/billings'
+    }).then(function(resp) {
+      console.log(resp.data);
+      $scope.billings = resp.data;
+    });
+  };
 
 }]);
