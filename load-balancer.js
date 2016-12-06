35ef320e96d149940d0cd3456c9fe5d2c823a443
lelea2 <daomaikhanh1988@yahoo.com>
2016-12-06 15:03:14 -0800

    Fixing load balancer

--- load-balancer.js
+++ load-balancer.js
@@ -1,27 +1,32 @@
-var arguments = process.argv.splice(2);
-var httpProxy = require('http-proxy');
+'use strict';
 
-//
-// Addresses to use in the round robin proxy
-//
-var addresses = [
-  {
-    host: 'localhost',
-    port: 8001
-  },
-  {
-    host: 'localhost',
-    port: 8002
-  },
-  {
-    host: 'localhost',
-    port: 8003
-  }
-];
+var http = require('http'),
+    httpProxy = require('http-proxy'),
+    proxy = httpProxy.createProxyServer({}),
+    url = require('url');
 
-var i = 0;
-httpProxy.createServer(function (req, res, proxy) {
-  console.log(addresses[i]);
-  proxy.proxyRequest(req, res, addresses[i]);
-  i = (i + 1) % addresses.length;
-}).listen(arguments[0] || 8000);
+http.createServer(function(req, res) {
+    var hostname = req.headers.host.split(':')[0];
+    var pathname = url.parse(req.url).pathname;
+
+    console.log(hostname);
+    console.log(pathname);
+    var rand = Math.floor(Math.random() * 3) + 1;
+
+    switch(rand) {
+      case 1:
+        console.log('Listing to port 8001');
+        proxy.web(req, res, { target: 'http://localhost:8001' });
+        break;
+      case 2:
+        console.log('Listing to port 8002');
+        proxy.web(req, res, { target: 'http://localhost:8002' });
+        break;
+      default:
+        console.log('Listing to port 8002');
+        proxy.web(req, res, { target: 'http://localhost:8003' });
+        break;
+    }
+}).listen(8000, function() {
+  console.log('proxy listening on port 8000');
+});
